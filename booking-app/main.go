89f6221f49f52227322f730955df8ff6cb9bbd12
manojreddykatta <manojkatta97@gmail.com>
2022-07-27 21:41:39 +0530

    initialization of go language

---
+++ booking-app/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("You can book a conference ticket from here")
+
+	var conferenceName = "Go conference"
+	const RemainingTickets = 50
+	fmt.Println(conferenceName)
+}
